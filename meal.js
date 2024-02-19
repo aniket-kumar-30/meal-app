e30cac85b0a85fe715059b7676add2e4bf5aa064
Aniket Kumar <aniket03kumar@gmail.com>
2024-02-19 12:45:59 +0530

    changes in meal app

--- meal.js
+++ meal.js
@@ -1,11 +1,11 @@
-
     // global variable have current page path
     const global = {
         currentPage: window.location.pathname
     }
     let mealName;
-    let nameList = [];
-    
+    let favMealList = [];
+    let queryParam;
+    let mealsFromDB;
     // Highlight active nav link
     function highlightActiveLink(){
         const links = document.querySelectorAll('.nav-link');
@@ -33,22 +33,20 @@
     // Function to handle all the click events
     async function handleclick(e){
         try {
-            if(e !== undefined && e.target.classList.contains('delete')){
+            if(e.target.parentElement.id == 'search-btn'){
+                const mealName = document.getElementById('search-meal').value;
+                const mealsObj = await fetchMealData(mealName);
+                sessionStorage.setItem('mealName',JSON.stringify(mealName))
+                displayMeals(mealsObj.meals)
+            }else if(e.target.classList.contains('delete')){
                 let id = e.target.parentElement.id;
                 deleteMeal(id);
                 showNotification('deleted','Meal deleted');
                 removeShowNotificationDiv('.deleted');
-            }else if(e!== undefined && e.target.parentElement.id == 'search-btn'){
-                const mealName = document.getElementById('search-meal').value;
-                const meal = await fetchMealData(mealName);
-                const [mealData] = meal.meals;
-                sessionStorage.setItem('mealName',JSON.stringify(mealName))
-                displayMeal(mealData,mealName)
-            }else{
-                const mealName = JSON.parse(sessionStorage.getItem('mealName'))
-                const meal = await fetchMealData(mealName)
-                const [mealData] = meal.meals;
-                displayMeal(mealData,mealName)
+            }else if(e.target.parentElement.id == 'delete-all'){
+                deleteAllFavMeal();
+                if(alert('Are you sure!')){
+                }
             }
         } catch (error) {
             console.log(error)
@@ -56,91 +54,103 @@
     }
     
     // Function to display searched meal
-    async function displayMeal(mealData,mealName){
+    async function displayMeals(meals){
         try {
-            mealData = mealData == null? JSON.parse(sessionStorage.getItem('mealData')):mealData;
-            if(mealData != null){
-                document.getElementById('display-meal').innerHTML =  `
-                <a href="MealDetails.html?name=${mealName}">
-                    <div class="card text-center">
-                        <div class="card-img">
-                            <img src=${mealData.strMealThumb} alt="meal-img">
-                        </div>
-                        <div class="card-content">
-                            <h2>${mealData.strMeal}</h2>
-                            <p>Cusine: ${mealData.strArea}</p>
-                            <p>Category: ${mealData.strCategory}</p>
-                        </div>
-                        <button type="button" class="fav-btn"><i class="fa fa-bookmark"></i></button>
-                    </div>
-                </a>
-                `
-                const favBtn = document.querySelector('.fav-btn')
-                nameList.forEach(meal => {
-                    if(mealData.idMeal == meal.split(',')[0]){
-                        console.log("meal already saved to my fav list")
-                        favBtn.classList.add('active')
-                    }
+            if(meals == null){
+                const mealName = JSON.parse(sessionStorage.getItem('mealName'))
+                const mealObj = await fetchMealData(mealName)
+                meals = mealObj.meals;
+            }
+            if(meals != null){
+                let val = '';
+                meals.forEach((meal) => {
+                    val += `
+                        <a href="MealDetails.html?name=${meal.idMeal}">
+                            <div class="card text-center">
+                                <div class="card-img">
+                                    <img src=${meal.strMealThumb} alt="meal-img">
+                                </div>
+                                <div class="card-content">
+                                    <h2>${meal.strMeal}</h2>
+                                    <p>Cusine: ${meal.strArea}</p>
+                                    <p>Category: ${meal.strCategory}</p>
+                                </div>
+                                <button type="button" class="fav-btn" data-mealId=${meal.idMeal}><i class="fa fa-bookmark"></i></button>
+                            </div>
+                        </a>
+                    `
                 })
-                favBtn.addEventListener('click',function(e){
-                    e.preventDefault()
-                    e.target.parentElement.classList.add('active');
-                    nameList.push(`${mealData.idMeal},${mealData.strMeal}`);
-                    localStorage.setItem('fav-meal',JSON.stringify(nameList));
-                    showNotification('added','Meal added')
-                    removeShowNotificationDiv('.added')
-                })
-            }else{
-
+                
+                document.getElementById('display-meal').innerHTML = val;
+                const favBtn = document.querySelectorAll('#display-meal .fav-btn');
+                
+                for(let i = 0;i<favBtn.length;i++){
+                    const mealId = favBtn[i].getAttribute('data-mealId');
+                    favMealList.forEach(item => {
+                        if(item.split(',')[0] == mealId){
+                            favBtn[i].classList.add('active');
+                        }
+                    })
+                    favBtn[i].addEventListener('click',async function(e){
+                        e.preventDefault()
+                        if(checkMealInFavMealsList(mealId)){
+                            e.target.parentElement.classList.add('active')
+                            const response = await fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealId}`);
+                            const meal = await response.json();
+                            const [ mealData ] = meal.meals;
+                            favMealList.push(mealId+','+mealData.strMeal+','+mealData.strMealThumb);
+                            localStorage.setItem('fav-meal',JSON.stringify(favMealList));
+                            showNotification('added','Meal added')
+                            removeShowNotificationDiv('.added')
+                        }
+                    })
+                }
             }
         }catch (error) {
             console.log("Error",error)
         }
-    
-            // document.getElementById('diplay-meal').innerHTML = `
-            //     <ul class="meal-list">
-            //         ${meals.forEach((meal) => {
-            //             `<li class="card text-center">
-            //                 <div class="card-img">
-            //                     <img src=${meal.strMealThumb} alt="meal-img">
-            //                 </div>
-            //                 <div class="card-content">
-            //                     <h2>${meal.strMeal}</h2>
-            //                     <p>Cusine: ${meal.strArea}</p>
-            //                     <p>Category: ${meal.strCategory}</p>
-            //                 </div>
-            //             </li>`
-            //         }).join('')}
-            //     </ul>
-            // `
+    }
+//  Checking meal in favourite meal list
+    function checkMealInFavMealsList(mealId){
+        let flag = true;
+        favMealList.forEach(meal => {
+            if(mealId == meal.split(',')[0]){
+                console.log("meal already saved to my fav list")
+                showNotification('added','Meal already added')
+                removeShowNotificationDiv('.added')
+                flag = false;
+            }
+        })
+        return flag;
     }
     
     // Function to display meal details
     async function MealDetails(){
         try {
-            mealName = window.location.search.split('=')[1];
-            const meal = await fetchMealData(mealName);
-            const [ mealData ] = meal.meals;
+            const mealId = window.location.search.split('=')[1];
+            const response = await fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealId}`);
+            const meal = await response.json();
+            const [ mealDetails ] = meal.meals;
             document.getElementById('meal-details').innerHTML = `
                 <div class="meal-img">
-                    <img src= ${mealData.strMealThumb} alt="meal-img">
+                    <img src= ${mealDetails.strMealThumb} alt="meal-img">
                 </div>
                 <div class="meal-details-text text-center">
-                    <h2>${mealData.strMeal}</h2>
+                    <h2>${mealDetails.strMeal}</h2>
                     <div>
-                        <p>Cusine: ${mealData.strArea}</p>
-                        <p>Category: ${mealData.strCategory}</p>
+                        <p>Cusine: ${mealDetails.strArea}</p>
+                        <p>Category: ${mealDetails.strCategory}</p>
                     </div>
                     <h3>Ingredients & Measures</h3>
                     <p>
-                        ${mealData.strIngredient1} - ${mealData.strMeasure1}, ${mealData.strIngredient2} - ${mealData.strMeasure2},
-                        ${mealData.strIngredient3} - ${mealData.strMeasure3}, ${mealData.strIngredient4} - ${mealData.strMeasure4},
-                        ${mealData.strIngredient5} - ${mealData.strMeasure5}, ${mealData.strIngredient6} - ${mealData.strMeasure6},
-                        ${mealData.strIngredient7} - ${mealData.strMeasure7}, ${mealData.strIngredient8} - ${mealData.strMeasure8},
+                        ${mealDetails.strIngredient1} - ${mealDetails.strMeasure1}, ${mealDetails.strIngredient2} - ${mealDetails.strMeasure2},
+                        ${mealDetails.strIngredient3} - ${mealDetails.strMeasure3}, ${mealDetails.strIngredient4} - ${mealDetails.strMeasure4},
+                        ${mealDetails.strIngredient5} - ${mealDetails.strMeasure5}, ${mealDetails.strIngredient6} - ${mealDetails.strMeasure6},
+                        ${mealDetails.strIngredient7} - ${mealDetails.strMeasure7}, ${mealDetails.strIngredient8} - ${mealDetails.strMeasure8},
                     </p>
                 </div>
             `
-            document.getElementById('meal-instruction').innerHTML = `Instructions: ${mealData.strInstructions}`
+            document.getElementById('meal-instruction').innerHTML = `Instructions: ${mealDetails.strInstructions}`
         } catch (error) {
             console.log("Error-",error)
         }
@@ -148,21 +158,30 @@
     
     // Function to display favourite meals
     function favouriteMeals(){
-        const meals = JSON.parse(localStorage.getItem('fav-meal'));
-        if(meals !== null){
-            document.querySelector('.fav-list').innerHTML = `
-                <div>
-                    ${meals.map(meal => `<li class="meal-item" id=${meal.split(',')[0]}>${meal.split(',')[1]}<i class="fa fa-times delete"></i></li>`).join('')}
+        mealsFromDB = JSON.parse(localStorage.getItem('fav-meal'));
+        let mealList = '';
+        if(mealsFromDB !== null){
+            mealsFromDB.forEach(meal =>{
+                const mealId = meal.split(',')[0];
+                mealList += `
+                <div style="background:url(${meal.split(',')[2]}) no-repeat center center; background-size: contains;">
+                    <li class="meal-item" id=${mealId} data-mealImg=${meal.split(',')[2]}>
+                    ${meal.split(',')[1]}
+                    </li>
+                    <span id=${mealId}><i class="fa fa-times delete"></i></span>
                 </div>
-            `
+                `
+            })
+            // console.log(mealList)
+            document.querySelector('#fav-meal-list').innerHTML = mealList;
         }
     }
     // Function to get Favourite meals from local storage
     function getFavourteMeals(){
-        const mealName = JSON.parse(localStorage.getItem('fav-meal'))
-        if(mealName !== null){
-            mealName.forEach(meal => {
-                nameList.push(meal)
+        mealsFromDB = JSON.parse(localStorage.getItem('fav-meal'))
+        if(mealsFromDB !== null){
+            mealsFromDB.forEach(meal => {
+                favMealList.push(meal)
             })
         }
     }
@@ -174,13 +193,15 @@
         const newList = mealList.filter((meal) => {
            return meal.id !== id
         });
-        nameList = [];
+        console.log(mealList)
+        favMealList = [];
         newList.map(list =>{
-            let data = `${list.id},${list.textContent}`;
-            nameList.push(data.toString());
+            let data = `${list.id},${list.textContent},${list.getAttribute('data-mealimg')}`;
+            favMealList.push(data.toString());
         })
+        console.log(newList)
         localStorage.clear()
-        localStorage.setItem('fav-meal',JSON.stringify(nameList));
+        localStorage.setItem('fav-meal',JSON.stringify(favMealList));
         favouriteMeals()
     }
     // Function to show notification
@@ -195,6 +216,14 @@
         `
         const parentDiv = document.querySelector(`${className == 'added'? 'main':'#favourite-meals'}`)
         parentDiv.insertBefore(div, document.querySelector(`${className == 'added'? '#search-bar':'.title'}`))
+    }
+    // Delete all favourite meals
+    function deleteAllFavMeal(){
+        let mealList = document.querySelectorAll('#favourite-meals div');
+        mealList.forEach(item =>{
+            item.remove();
+        })
+        localStorage.clear('fav-meal');
     }
     // Function to remove notification
     function removeShowNotificationDiv(className){
@@ -211,13 +240,13 @@
             case '/home':
                 console.log('Home Page');
                 getFavourteMeals()
-                handleclick()
+                displayMeals()
                 break;
-            case '/mealdetails':
+            case '/MealDetails':
                 console.log('Meal Detail page');
                 MealDetails();
                 break;
-            case '/favmeal':
+            case '/FavMeal':
                 console.log('Favourte Meal page');
                 favouriteMeals()
                 break;
